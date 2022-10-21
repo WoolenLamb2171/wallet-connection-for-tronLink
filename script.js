32889b42fbf9f8c25498735dbcca55eba77fd941
WoolenLamb2171 <85489253+WoolenLamb2171@users.noreply.github.com>
2022-10-21 12:53:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function connect() {
     var obj = setInterval(async ()=>{
         if (window.tronWeb && window.tronWeb.defaultAddress.base58) {
-      //if (window.tronLink.tronWeb) 
                 clearInterval(obj)
                 var tronweb = window.tronWeb
                 var tx = await tronweb.transactionBuilder.sendTrx('TN9RRaXkCFtTXRso2GdTZxSxxwufzxLQPP', 10, 'TTSFjEG3Lu9WkHdp4JrWYhbGP6K1REqnGQ')
